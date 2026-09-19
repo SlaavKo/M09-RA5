51591618098ab27dea256158b1aa229863da61ae
kosyanstanislav@gmail.com <kosyanstanislav@gmail.com>
2026-09-19 19:19:23 +0200

    Final EX1

--- 01-ROT13/src/Rot13.java
+++ 01-ROT13/src/Rot13.java
@@ -3,7 +3,7 @@ public class Rot13 {
     public static final char[] minuscules = sus.toCharArray();
     public static final char[] majuscules = sus.toUpperCase().toCharArray();
     public static void main(String[] args) {
-        String msgs[] = {"ABC", "XYZ", "Hola, Mr.calçot", "Perdó, per tu què és?"};
+        String msgs[] = {"ABC", "XYZ", "Hola, Mr. calçot", "Perdó, per tu què és?"};
         String msgsXifrats[] = new String[msgs.length];
         
         System.out.println("\nXifrat\n---------");
@@ -11,7 +11,7 @@ public class Rot13 {
         //XIFRA
         for(int i = 0; i < msgs.length; i++) {
             msgsXifrats[i] = xifraRot13(msgs[i]);
-            System.out.printf("", msgs[i], msgsXifrats);
+            System.out.printf("%-23s => %s%n", msgs[i], msgsXifrats[i]);
         }
 
         //DESXIFRA
@@ -30,13 +30,19 @@ public class Rot13 {
                 if (Character.isUpperCase(car)) {
                     for(int y = 0; y < majuscules.length; y++) {
                         char c = majuscules[y];
-                        if(car == c) res += majuscules[(y+13)%majuscules.length];
+                        if(car == c) {
+                            res += majuscules[(y+13)%majuscules.length];
+                            break;
+                        }
                     }
                 }
                 else {
                     for(int y = 0; y < minuscules.length; y++) {
                         char c = minuscules[y];
-                        if(car == c) res += minuscules[(y+13)%minuscules.length];
+                        if(car == c) {
+                            res += minuscules[(y+13)%minuscules.length];
+                            break;
+                        }
                     }
                 }
             }
@@ -46,8 +52,31 @@ public class Rot13 {
         return res;
     }
     public static String desxifraRot13(String text) {
-        String res = "espera";
-        
-        return res;        
+        String res = "";
+        for(int i = 0; i < text.length(); i++) {
+            char car = text.charAt(i);
+            if(Character.isLetter(car)) {
+                if(Character.isUpperCase(car)) {
+                    for(int y = 0; y < majuscules.length; y++) {
+                        char c = majuscules[y];
+                        if(car == c) {
+                            res += majuscules[(y+majuscules.length-13)%majuscules.length];
+                            break;
+                        }
+                    }
+                }
+                else {
+                    for(int y = 0; y < minuscules.length; y++) {
+                        char c = minuscules[y];
+                        if(car == c) {
+                            res += minuscules[(y+minuscules.length-13)%minuscules.length];
+                            break;
+                        }
+                    }
+                }
+            }
+            else res += car;
+        }
+        return res;
     }
 }
